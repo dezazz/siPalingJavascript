deaa7ec0ad1b8dbf2904c2cce8af792015215371
LAPTOP-5S5L0NU2\USER <dezabanjarnahor@gmail.com>
2023-02-22 22:13:57 +0700

    first commit

---
+++ part 1/script.js
@@ -0,0 +1,13 @@
+let js="amazing";
+// if ( js==="amazing") alert ("Javascript is FUN");
+
+console.log("deza");
+console.log("indonesia")
+
+let namek = "deza";
+let age = 21 ;
+let home ="perbaungan";
+
+console.log(namek);
+console.log(age);
+console.log(home);
